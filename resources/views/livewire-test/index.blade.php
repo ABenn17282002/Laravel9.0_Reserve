6a60986cf93426f1f1888947e812fae93f66db21
a_yamamoto <a.benn17282002@gmail.com>
2022-04-26 00:06:10 +0900

    Livewire_component準備

--- resources/views/livewire-test/index.blade.php
+++ resources/views/livewire-test/index.blade.php
@@ -1,1 +1,11 @@
-Live-WireTestです!
+<html>
+    <head>
+        {{-- LiveWire使用のためのlayouts設定 --}}
+        @livewireStyles
+    </head>
+    <body>
+        Live-WireTestです!
+
+        @livewireScripts
+    </body>
+</html>
