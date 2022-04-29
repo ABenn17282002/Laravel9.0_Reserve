e6db5741a55f47063cd7b6f78c99c79a18d1b752
a_yamamoto <a.benn17282002@gmail.com>
2022-04-29 22:16:58 +0900

    Livewire_lifecycle

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -14,6 +14,18 @@ class Counter extends Component
 
     public $name3="";
 
+    // 描画実行前処理
+    public function mount()
+    {
+        $this->name2 = 'mount';
+    }
+
+    // データ更新毎
+    public function updated()
+    {
+        $this->name2 = 'updated';
+    }
+
     public function increment()
     {
         $this->count++;
