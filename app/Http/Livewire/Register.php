f5b71cb4b1a734a3528e26d709a095be76505417
a_yamamoto <a.benn17282002@gmail.com>
2022-04-29 23:32:01 +0900

    form_preparation(2)

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -6,9 +6,14 @@ use Livewire\Component;
 
 class Register extends Component
 {
+    // formの値を格納する変数
+    public $name;
+    public $email;
+    public $password;
+
     public function register()
     {
-        dd('登録テスト');
+        dd($this);
     }
 
     public function render()
--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,6 +1,14 @@
 <div>
     {{-- ページ読み込み防止処理 --}}
     <form wire:submit.prevent="register">
+        <label for="name">名前</label>
+        <input id="name" type="text" wire:model="name"><br>
+
+        <label for="email">メールアドレス</label>
+        <input id="email" type="email" wire:model="email"><br>
+
+        <label for="password">パスワード</label>
+        <input id="password" type="password" wire:model="password"><br>
         <button>登録</button>
     </form>
 </div>
