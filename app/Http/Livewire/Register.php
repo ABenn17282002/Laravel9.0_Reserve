23db191e790abe25f26bf942026bbe114583ddf8
a_yamamoto <a.benn17282002@gmail.com>
2022-04-30 22:58:54 +0900

    form_Validation

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+// User・Hashモデルの使用
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Component
 {
@@ -11,9 +14,25 @@ class Register extends Component
     public $email;
     public $password;
 
+    // validation
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+    ];
+
+    // 登録処理
     public function register()
     {
-        dd($this);
+        $this->validate();
+        // dd($this);
+
+        // DBへ登録
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
     }
 
     public function render()
