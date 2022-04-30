5c4d617e97532f304c45e01ab9ee4976fd4e24f6
a_yamamoto <a.benn17282002@gmail.com>
2022-04-30 23:09:18 +0900

    Validation_message

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -3,12 +3,15 @@
     <form wire:submit.prevent="register">
         <label for="name">名前</label>
         <input id="name" type="text" wire:model="name"><br>
+        @error('name')<div>{{ $message }}</div> @enderror
 
         <label for="email">メールアドレス</label>
         <input id="email" type="email" wire:model="email"><br>
+        @error('email')<div>{{ $message }}</div> @enderror
 
         <label for="password">パスワード</label>
         <input id="password" type="password" wire:model="password"><br>
+        @error('password')<div>{{ $message }}</div> @enderror
         <button>登録</button>
     </form>
 </div>
