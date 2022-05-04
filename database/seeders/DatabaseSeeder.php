3fc305b10f604cbde5e4bd57d973e959c1cdf170
a_yamamoto <a.benn17282002@gmail.com>
2022-05-04 15:30:22 +0900

    roleの設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // ダミーデータの登録
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
