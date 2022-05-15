d6103b7589ded3d43563ac7a9f8fff8dcf9a370c
a_yamamoto <a.benn17282002@gmail.com>
2022-05-15 22:03:36 +0900

    flatpickrの日本語化・時間表示

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -1,5 +1,28 @@
 // es modules are recommended, if available, especially for typescript
 import flatpickr from "flatpickr";
+// flatpickrの日本語化用モジュール
+import { Japanese } from "flatpickr/dist/l10n/ja.js"
 
 // Otherwise, selectors are also supported
-flatpickr("#event_date", {});
+flatpickr("#event_date", {
+    "locale":Japanese, // 日本
+    minDate:"today",   // 最少日時(今日)
+    maxDate: new Date().fp_incr(30)  //今日から何日選択可能か
+});
+
+// 開始時間と終了時間の設定
+const setting = {
+    "locale": Japanese, // 日本語
+    enableTime: true,   // 時間表示
+    noCalendar: true,   // calender表示なし
+    dateFormat: "H:i",  // ●時▲分表示
+    time_24hr: true,    // 24時間表示
+    minTime: "10:00",   // <開始>10:00
+    maxTime: "20:00",   // <終了>20:00
+  }
+
+// 開始時間
+flatpickr("#start_time", setting);
+// 終了時間
+flatpickr("#end_time", setting);
+
