96362aaaf0725287d00dd883194a2c24881567d0
a_yamamoto <a.benn17282002@gmail.com>
2022-05-15 21:16:37 +0900

    flatpickrの導入

---
+++ resources/js/flatpickr.js
@@ -0,0 +1,5 @@
+// es modules are recommended, if available, especially for typescript
+import flatpickr from "flatpickr";
+
+// Otherwise, selectors are also supported
+flatpickr("#event_date", {});
