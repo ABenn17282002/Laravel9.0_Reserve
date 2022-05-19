eeabfc28102cc1d89a481294583d26c37e3db38c
a_yamamoto <a.benn17282002@gmail.com>
2022-05-20 00:29:28 +0900

    edit

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
-
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}></textarea>
+{{-- slotの記述がないと文字が表示されない --}}
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>{{ $slot }}</textarea>
