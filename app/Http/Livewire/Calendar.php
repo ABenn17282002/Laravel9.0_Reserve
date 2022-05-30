e60c5e23e0a656ae88e9025cda46459d83275bc3
a_yamamoto <a.benn17282002@gmail.com>
2022-05-30 22:43:10 +0900

    日付の変更

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -29,6 +29,22 @@ class Calendar extends Component
         // dd($this->currentWeek);
     }
 
+    // 日付取得用関数
+    public function getDate($date)
+    {
+        // 現在日時の文字列化
+        $this->currentDate = $date;
+        $this->currentWeek =[];
+
+        for($i=0; $i < 7; $i++)
+        {
+            // parseでCarbonインスタンスに変換後、日付変換
+            $this->day = Carbon::parse($this->currentDate)->addDays($i)->format('m月d日');
+            // 連想配列に追加
+            \array_push($this->currentWeek, $this->day);
+        }
+    }
+
     public function render()
     {
         return view('livewire.calendar');
--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,6 +1,9 @@
 <div>
     カレンダー
-    <x-jet-input id="calendar" class="block mt-1 w-full" type="text" name="calendar" />
+    <x-jet-input id="calendar" class="block mt-1 w-full" type="text" name="calendar"
+    {{-- datepickerで変更したら値も変更する --}}
+    value="{{ $currentDate }}"
+    wire:change="getDate($event.target.value)"/>
     {{ $currentDate }}
     <div class="flex">
         {{-- 日付の取得  --}}
