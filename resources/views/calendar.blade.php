590f65baf8f222ca2331dfd1604c94abf37ba109
a_yamamoto <a.benn17282002@gmail.com>
2022-06-07 23:26:15 +0900

    Blade_Component(day,calendar-time)

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,7 +6,8 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {{-- 追加のCSSを記載 ※要:npm run watch --}}
+        <div class="event-calendar border border-red-400 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('calendar')
             </div>
--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,9 +1,24 @@
 <div>
-    カレンダー
-    <x-jet-input id="calendar" class="block mt-1 w-full" type="text" name="calendar"
+    <div class="text-center text-sm">
+        日付を選択してください。本日から最大30日先まで選択可能です。
+    </div>
+    <x-jet-input id="calendar" class="block mt-1 mb-2 mx-auto" type="text" name="calendar"
     {{-- datepickerで変更したら値も変更する --}}
     value="{{ $currentDate }}"
     wire:change="getDate($event.target.value)"/>
+
+    <div class="flex border border-green-400 mx-auto">
+        <x-calendar-time /> <!-- コンポーネント作成 仮で直書き -->
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+    </div>
+
+
     {{ $currentDate }}
     <div class="flex">
         {{-- 日付の取得  --}}
