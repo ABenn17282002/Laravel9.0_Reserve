61cea202aa14cb8a48474e0d8488947c6519c484
a_yamamoto <a.benn17282002@gmail.com>
2022-06-23 23:21:30 +0900

    予約可能人数の表示

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+// DBファザード
+use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
@@ -16,7 +18,28 @@ class ReservationController extends Controller
     {
         // eventIDを取得
         $event = Event::findOrFail($id);
-        // eventIDを詳細ページに渡す
-        return \view('event-detail',\compact('event'));
+
+        $reservedPeople = DB::table('reservations')
+        // select内でsumを使うため、クエリビルダのDB::rawで対応
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        // キャンセル分を合計から除外する
+        ->whereNull('canceled_date')
+        ->groupBy('event_id')
+        // havingはgroupBy後の検索
+        ->having('event_id',$event->id)
+        ->first();
+
+        // 予約者がnullでなければ
+        if(!is_null($reservedPeople))
+        {
+            // 予約可能な人数 = 最大定員 - 予約済みの人数 (キャンセルを除く)
+            $resevablePeople = $event->max_people - $reservedPeople->number_of_people;
+        }
+        else {
+            $resevablePeople = $event->max_people;
+        }
+
+        // eventIDと予約可能人数を詳細ページに渡す
+        return \view('event-detail',\compact('event','resevablePeople'));
     }
 }
